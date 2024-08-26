e19580eba5d95749ceacd9d36db35e6965ebea4a
mjr2595 <michaeljohnraymond@gmail.com>
2024-08-25 22:37:51 -0500

    spawnin some astroids

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame
 
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from player import Player
 
@@ -10,7 +12,19 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Asteroid.containers = (asteroids, updatable, drawable)  # type: ignore
+    AsteroidField.containers = updatable  # type: ignore
+    asteroid_field = AsteroidField()
+
+    Player.containers = (updatable, drawable)  # type: ignore
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     dt = 0
 
     running = True
@@ -26,8 +40,10 @@ def main():
         screen.fill("black")
 
         # RENDER YOUR GAME HERE
-        player.update(dt)
-        player.draw(screen)
+        for sprite in updatable:
+            sprite.update(dt)
+        for sprite in drawable:
+            sprite.draw(screen)
 
         # flip() the display to put your work on screen
         pygame.display.flip()
